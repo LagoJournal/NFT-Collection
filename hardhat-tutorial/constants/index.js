e087cacd773c2b127da16e0a34ba6e13480222eb
LagoJournal <agustin.lago@mi.unc.edu.ar>
2022-09-30 18:22:23 -0300

    NFT-Collection done

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x6a619944066f2ee05Ea95f56F5EeA04CA2528925";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
